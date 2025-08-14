fafb598397b9bc6569e31f69461f86b552d4e653
Romi123 <romicoolvarghese@gmail.com>
2025-08-14 09:57:26 +0400

    Updated the code which can run on all 3 env with test data

--- cypress.config.js
+++ cypress.config.js
@@ -1,31 +1,22 @@
 import { defineConfig } from 'cypress';
 import mochawesome from 'cypress-mochawesome-reporter/plugin.js';
+import path from 'path';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://www.saucedemo.com',
     setupNodeEvents(on, config) {
       mochawesome(on);
 
-      const environment = config.env.ENV || 'prod';
-      const urls = {
-        qa: 'https://hub.qa.saucedemo.com',
-        staging: 'https://hub.stag.saucedemo.com',
-        prod: 'https://www.saucedemo.com'
-      };
-      config.baseUrl = urls[environment];
+      const envName = config.env.ENV || 'qa';
+      const envConfig = require(path.resolve(`./cypress/config/${envName}.json`));
+
+      Object.assign(config.env, envConfig);
+      config.baseUrl = envConfig.baseUrl;
 
       return config;
-    },
-  },
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir: 'cypress/reports',
-    overwrite: false,
-    html: true,
-    json: true,
-    charts: true,
-    embeddedScreenshots: true, // optional but recommended
-    inlineAssets: true
-  },
+    }
+  }
 });
